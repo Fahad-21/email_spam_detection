6a3921a448394087ba3741aab5dad67d95f40a39
Fahad Ali <alifahad370@gmail.com>
2022-12-12 19:38:32 +0000

    Reading emails

--- main.py
+++ main.py
@@ -138,3 +138,20 @@ def gmail_authenticate():
 
 # get the Gmail API service
 service = gmail_authenticate()
+
+
+# Reading emails with specific content.
+
+# utility functions
+
+def search_messages(service, query):
+    result = service.users().messages().list(userId='me',q=query).execute()
+    messages = [ ]
+    if 'messages' in result:
+        messages.extend(result['messages'])
+    while 'nextPageToken' in result:
+        page_token = result['nextPageToken']
+        result = service.users().messages().list(userId='me',q=query, pageToken=page_token).execute()
+        if 'messages' in result:
+            messages.extend(result['messages'])
+    return messages
